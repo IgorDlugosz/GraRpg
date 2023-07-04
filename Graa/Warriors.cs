b9856cf126825bd7e78bf4dfc4728aea4b119562
Igor <Igor@DESKTOP-TNFBOQ5>
2023-07-04 14:30:09 +0200

    koncowka

--- Graa/Warriors.cs
+++ Graa/Warriors.cs
@@ -1,9 +1,4 @@
 ﻿using Graa;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Graa
 {
@@ -13,32 +8,34 @@ namespace Graa
         private List<Warrior> warriors;
         public void KWar()
         {
-            Console.WriteLine("Podaj liczbe wojownikow: ")
+            Console.WriteLine("Podaj liczbe wojownikow: ");
 
 
 numWar = int.Parse(Console.ReadLine());
 
         }
-   public void genWar()
+        public void genWar()
         {
-            warriors= new List<Warrior>();
-       
-        Random random = new Random();
-for (int i=0;i< numOfWarriors; i++) {
-int typ = random.nextInt(3);
-if (typ==0) {
-warriors.add(new Warrior(Łucznik; + (i+1), 90, 30));
-} 
+            warriors = new List<Warrior>();
+
+            Random random = new Random();
+            for (int i = 0; i < numWar; i++)
+            {
+                int typ = random.Next(3);
+                if (typ == 0)
+                {
+                    warriors.Add(new Warrior($"Łucznik {(i + 1)}", 90, 30));
+        } 
         else if (typ==1) {
-warriors.add(new Warrior(Mag; + (i+1), 100, 25));
+warriors.Add(new Warrior($"Mag; +{ (i+1)}", 100, 25));
 } 
     else if (typ == 2)
 {
-    warriors.add(new Warrior( Nekromanta ; +(i + 1), 150, 15));
+    warriors.Add(new Warrior($"Nekromanta(i + 1)" , 150, 15));
 }}
-for (Warrior warrior in warriors)
+foreach (Warrior warrior in warriors)
 {
-    Console.WriteLine(warrior.getName() + "ma" + warrior.getHp() + "hp") ;
+    Console.WriteLine(warrior.getName() + "ma" + warrior.getHp() + "hp");
 }}}}
 
     
