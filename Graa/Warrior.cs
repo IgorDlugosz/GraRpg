dc02f3543bb07270e9fc7890efcb70c54d3a8f70
Igor <Igor@DESKTOP-TNFBOQ5>
2023-07-04 12:54:16 +0200

    Dodaj pliki projektów.

---
+++ Graa/Warrior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graa
+{
+    internal class Warrior
+    {
+        private String name;
+        private int hp;
+        private int dam;
+        public Warrior(String name, int hp, int dam)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.dam = dam;
+        }
+        public String getName()
+        {
+            return name;
+        }
+        public int getHp() {
+            return hp;
+        } 
+    public int getDam()
+        {
+            return dam;
+        }
+    public void takeDam(int dam) {
+            hp = hp - dam;
+        if (hp < 0)
+            {
+                hp = 0;
+            }
+        
+        }
+    
+    
+    }
+}
